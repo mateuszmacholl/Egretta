a40e14eb3f913b033d13117baa696ee21c44b1e8
mateuszmacholl <mateuszmacholl0@gmail.com>
2018-09-23 21:29:13 +0200

    fix secret

--- src/main/kotlin/mateuszmacholl/egretta/configuration/jwt/SecurityConstants.kt
+++ src/main/kotlin/mateuszmacholl/egretta/configuration/jwt/SecurityConstants.kt
@@ -1,7 +1,7 @@
 package mateuszmacholl.egretta.configuration.jwt
 
 object SecurityConstants {
-    const val SECRET = "SeceretSecretSecretKey"
+    const val SECRET = "SecretSecretSecretKey"
     const val EXPIRATION_TIME: Long = 864000000 // 10 days
     const val TOKEN_PREFIX = "Bearer "
     const val HEADER_STRING = "Authorization"
