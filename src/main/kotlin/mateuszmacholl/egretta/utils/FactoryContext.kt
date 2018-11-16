59f4af8f07d38ca1272c4c3b6b5fc8cf047786c3
mateuszmacholl <mateuszmacholl0@gmail.com>
2018-11-17 00:19:47 +0100

    change list visibility in factory context class

--- src/main/kotlin/mateuszmacholl/egretta/utils/FactoryContext.kt
+++ src/main/kotlin/mateuszmacholl/egretta/utils/FactoryContext.kt
@@ -4,16 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired
 
 // I have no idea in which package it should be
 abstract class FactoryContext<type> {
-    protected var list: MutableList<type> = mutableListOf()
+    private var list: MutableList<type> = mutableListOf()
+        @Autowired
+        private set(list)  {
+            field = list
+        }
 
     fun <conversionGoal  : type> get(typeClass: Class<conversionGoal>): conversionGoal{
         val goal = list.firstOrNull { typeClass.isInstance(it) } ?: throw IllegalArgumentException()
         @Suppress("UNCHECKED_CAST") // safety is implied by the high-level program logic
         return goal as conversionGoal
-    }
-
-    @Autowired
-    fun set(list: MutableList<type>){
-        this.list = list
     }
 }
